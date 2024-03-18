cfac2558b80e64769f969b94d299c0206a2ae075
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2024-03-18 13:09:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def hello():
+    return "Flask inside Docker!!"
+
 
 if __name__ == "__main__":
-   app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
